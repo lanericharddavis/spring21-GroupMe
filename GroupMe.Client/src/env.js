9feae67e43352366d85c5cdb5348779a048e741d
Lane <lanericharddavis@gmail.com>
2021-05-26 13:45:07 -0600

    inital push

--- GroupMe.Client/src/env.js
+++ GroupMe.Client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'https://localhost:5001' : ''
-export const domain = 'codeworksclassroom.auth0.com'
-export const audience = 'https://codeworksclassroom.com'
-export const clientId = 'pOXw2OGv1LsYi7LEBmDF04RLkXQvldml'
+export const domain = 'lanerd.us.auth0.com'
+export const audience = 'https://myKanbanApi.com'
+export const clientId = 'MrpO1Gkua5NxFhPG5U28Lksb5xPTWGU7'
