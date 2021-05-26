bea31c4f5c52e323676986e1ca13b344b3fe7b4a
Lane <lanericharddavis@gmail.com>
2021-05-26 17:17:09 -0600

    eod

--- GroupMe.Server/DbSetup.sql
+++ GroupMe.Server/DbSetup.sql
@@ -26,6 +26,24 @@ CREATE TABLE IF NOT EXISTS group_members(
   FOREIGN KEY (accountId) REFERENCES accounts(id) ON DELETE CASCADE,
   FOREIGN KEY (groupId) REFERENCES groups(id) ON DELETE CASCADE
 ) default charset utf8 COMMENT '';
+CREATE TABLE IF NOT EXISTS comments(
+  id INT AUTO_INCREMENT NOT NULL primary key COMMENT 'primary key',
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  creatorId varchar(255) NOT NULL COMMENT 'FK: User Account',
+  body varchar(255) DEFAULT "empty comment" COMMENT 'Comment Body',
+  groupId int NOT NULL COMMENT 'FK: Group',
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE,
+  FOREIGN KEY (groupId) REFERENCES groups(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
+INSERT INTO
+  comments(body, groupId, creatorId)
+VALUES
+  (
+    "This is the groups first comment!",
+    3,
+    "cd5f28ba-d192-4c30-9434-b39c95fdfdfa"
+  );
 INSERT INTO
   groups(name, description, img, creatorId)
 VALUES
@@ -33,7 +51,7 @@ VALUES
     "CodeWorks Alumni",
     "Let's talk code!!!",
     "//placehold.it/500x500",
-    "358afb43-c49f-43b4-a3aa-ee24cec2b35e"
+    "cd5f28ba-d192-4c30-9434-b39c95fdfdfa"
   );
 SELECT
   g.*,
@@ -44,4 +62,15 @@ FROM
   groups g
   JOIN accounts a ON a.id = g.creatorId
 WHERE
-  g.id = 1;
+  g.id = 3;
+SELECT
+  c.*,
+  g.*,
+  a.name,
+  a.picture
+FROM
+  comments c
+  JOIN accounts a ON c.creatorId = a.id;
+  JOIN groups g ON c.groupId = g.id;
+WHERE
+  groupId = 3;
